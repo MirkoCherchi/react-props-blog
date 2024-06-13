035b29f0a4bc921ce87050ee1170176ca1ecdaae
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-06-13 15:13:27 +0200

    feat: color tags

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,21 +1,32 @@
 const Card = ({ post }) => {
   return (
-    <div className="post-card">
-      <figure className="post-card-image">
-        <img src={post.image} alt="Post" />
-      </figure>
-      <div className="post-card-content">
-        <h2>{post.title}</h2>
-        <p>{post.content}</p>
-        <div>
-          <small>
-            <strong>Tags: </strong>
-            {post.tags.join(" - ")}
-          </small>
+    <>
+      {post.published && (
+        <div className="post-card">
+          <figure className="post-card-image">
+            <img
+              src={post.image || "https://placehold.co/600x400/png"}
+              alt="Post"
+            />
+          </figure>
+          <div className="post-card-content">
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <div className="divTag">
+              <small>
+                <strong>Tags: </strong>
+                {post.tags.map((tag, index) => (
+                  <span key={index} className={`tag ${tag}`}>
+                    #{tag}
+                  </span>
+                ))}
+              </small>
+            </div>
+            <button>LEGGI DI PIÙ</button>
+          </div>
         </div>
-        <button>LEGGI DI PIÙ</button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
