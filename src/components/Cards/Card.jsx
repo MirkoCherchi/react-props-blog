2d5049411a6d8f200bd2fad20e6e445c272a935a
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-06-13 13:05:27 +0200

    feat: props

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,20 +1,18 @@
-import colosseoPost from "../../assets/img/colosseo.jpg";
-
-const Card = () => {
+const Card = ({ post }) => {
   return (
     <div className="post-card">
       <figure className="post-card-image">
-        <img src={colosseoPost} alt="Post" />
+        <img src={post.image} alt="Post" />
       </figure>
       <div className="post-card-content">
-        <h2>Colosseo</h2>
-        <p>
-          Il Colosseo, costruito nell'80 d.C., è il più grande anfiteatro del
-          mondo antico, situato nel cuore di Roma. Con una capacità di 50.000
-          spettatori, ospitava giochi gladiatori e spettacoli pubblici.
-          Nonostante i danni subiti nel tempo, rimane un simbolo iconico della
-          città eterna e un'importante attrazione turistica.
-        </p>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
+        <div>
+          <small>
+            <strong>Tags: </strong>
+            {post.tags.join(" - ")}
+          </small>
+        </div>
         <button>LEGGI DI PIÙ</button>
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,14 @@
 import Card from "./Cards/Card";
+import { posts } from "../posts";
 const Main = () => {
   return (
     <>
       <main className="container">
-        <Card />
+        <div className="flex-card">
+          {posts.map((post) => (
+            <Card key={post.id} post={post} />
+          ))}
+        </div>
       </main>
     </>
   );
